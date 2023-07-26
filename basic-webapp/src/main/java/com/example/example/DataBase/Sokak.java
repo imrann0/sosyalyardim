6989919811a9c14ba46cf33b3319a3db000a1cb2
Aliimran <aliimranbozkurt61y@gmail.com>
2023-07-26 15:26:01 +0300

    Sokak Mahalle Tabloları Oluşturuldu Veriler Eklendi muracatt işlemleri geliştirildi.

---
+++ basic-webapp/src/main/java/com/example/example/DataBase/Sokak.java
@@ -0,0 +1,78 @@
+package com.example.example.DataBase;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.TypedQuery;
+
+@Entity
+public class Sokak {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sokakID;
+    private String sokakkod;
+    private String sokakname;
+    
+    @ManyToOne
+    @JoinColumn(name = "district_id")
+    private District district;
+
+    // Getter methods
+    public int getSokakID() {
+        return sokakID;
+    }
+
+    public String getSokakkod() {
+        return sokakkod;
+    }
+
+    public String getSokakname() {
+        return sokakname;
+    }
+
+    // Setter methods
+    public void setSokakID(int sokakID) {
+        this.sokakID = sokakID;
+    }
+
+    public void setSokakkod(String sokakkod) {
+        this.sokakkod = sokakkod;
+    }
+
+    public void setSokakname(String sokakname) {
+        this.sokakname = sokakname;
+    }
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
+    }
+    
+    public static List<Sokak> getAllSokak() {
+        Configuration configuration = new Configuration().configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        TypedQuery<Sokak> query = session.createQuery("SELECT s FROM Sokak s", Sokak.class);
+        List<Sokak> Sokak = query.getResultList();
+
+        session.getTransaction().commit();
+        sessionFactory.close();
+
+        return Sokak;
+    }
+}
